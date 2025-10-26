c3125cc1ebb8e1ee234867e5478ae24ca912a7dd
Jed Saxon <jed-sax@proton.me>
2025-10-26 17:05:43 +1100

    make default config better for what i'm about to do

--- config/config.go
+++ config/config.go
@@ -9,16 +9,29 @@ type Config struct {
 	ListenAddres string
 }
 
-func NewEmptyConfig() *Config {
+const (
+	defaultSqliteDsn     = ":memory"
+	defaultListenAddress = "127.0.0.1:5000"
+)
+
+func NewDefaultConfig() *Config {
 	return &Config{
-		SqliteDsn:    "",
-		ListenAddres: "",
+		SqliteDsn:    defaultSqliteDsn,
+		ListenAddres: defaultListenAddress,
 	}
 }
 
 func (c *Config) PopulateFromArgs() {
-	flag.StringVar(&c.SqliteDsn, "sqlite", ":memory:", "where the sqlite database should be stored")
-	flag.StringVar(&c.ListenAddres, "listen", "127.0.0.1:5000", "where this program should listen for api requests")
+	var sqliteDsn = flag.String("sqlite", "", "where the sqlite database should be stored")
+	var listenAddress = flag.String("listen", "", "where this program should listen for api requests")
 
 	flag.Parse()
+
+	if *sqliteDsn != "" {
+		c.SqliteDsn = *sqliteDsn
+	}
+
+	if *listenAddress != "" {
+		c.ListenAddres = *listenAddress
+	}
 }
