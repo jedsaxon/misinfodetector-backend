d999f67d82e4c4e6714cc982c796bae4d1079c26
Jed Saxon <jed-sax@proton.me>
2025-10-26 17:05:41 +1100

    Move initDb into dbservice.go

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 )
 
 func main() {
+	log.Printf("connecting and initialising sqlite")
 	db, err := sql.Open("sqlite3", "file:/var/lib/backend/app.db?cache=shared")
 	if err != nil {
 		log.Fatalf("error opening sqlite database: %v", err)
@@ -22,10 +23,7 @@ func main() {
 	defer db.Close()
 	dbs := dbservice.NewDbService(db)
 
-	log.Printf("initialising sqlite")
-	initDb(db)
-
-	c := handler.NewPostsController(dbs) 
+	c := handler.NewPostsController(dbs)
 	r := mux.NewRouter()
 
 	r.Use(middleware.LoggingMiddleware)
@@ -41,12 +39,4 @@ func main() {
 	if err != nil {
 		log.Fatalf("error while listening for requests: %v", err)
 	}
-}
-
-func initDb(db *sql.DB) error {
-	_, err := db.Exec("create table if not exists posts(id varchar(36), message text, username text, date_submitted text, is_misinformation int);")
-	if err != nil {
-		return err
-	}
-	return nil
 }
